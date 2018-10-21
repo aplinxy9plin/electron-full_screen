cbb912d0b9946762ee9bb5c71da9a4007cf3d4c0
AplinXy9plin <aplinxy9plin@icloud.com>
2018-10-22 02:22:35 +1200

    Fix blank page

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
-
+app.commandLine.appendSwitch("disable-gpu")
+app.commandLine.appendArgument("disable-gpu")
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
