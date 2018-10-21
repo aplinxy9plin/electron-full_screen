ec0c80a0d2b10c1ce794ccb799c5658dd69f3d01
AplinXy9plin <evilberry@icloud.com>
2018-10-21 21:00:04 +0700

    Add time widget

--- main.js
+++ main.js
@@ -13,7 +13,9 @@ function createWindow () {
        minHeight: 610,
        minWidth: 561
      });
-  // mainWindow.setFullScreen(true)
+  // for the full screen!!!
+  mainWindow.setFullScreen(true)
+
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
   // Open the DevTools.
